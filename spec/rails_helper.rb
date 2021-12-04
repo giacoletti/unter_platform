7a1c6ab7d6b66be1f6d2873f292d45fec1dc8c2a
giacoletti <giacoletti92@gmail.com>
2021-12-04 18:10:00 +0100

    Configures Shoulda Matchers, Response JSON abbreviation, Factory Bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,9 +12,14 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
+  config.include Shoulda::Matchers::ActiveRecord, type: :model
+  config.include ResponseJSON
 end
